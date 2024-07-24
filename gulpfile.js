a80555576f0730aee7a73be5027dd2ab522f7966
ronny <ronnymijiazuleta@gmail.com>
2024-07-24 10:41:04 -0500

    Se realizaron cambios

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,6 @@ import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
-import terser from 'gulp-terser'
-
-
-export function js(done) {
-    console.log('Iniciando la tarea js...'); // Indicar que la tarea js ha comenzado
-    src('src/js/app.js', { allowEmpty: true })
-        .pipe(terser())
-        .pipe(dest('build/js'))
-        .on('end', () => console.log('Tarea js completada.')); // Confirmar que la tarea js ha terminado
-    done();
-}
-
 export function css( done ) {
     src('src/scss/app.scss', {sourcemaps: true})
         .pipe( sass({
@@ -28,9 +16,8 @@ export function css( done ) {
 }
 
 function watchFiles() {
-    watch('src/js/**/*.js', js); // Vigila los archivos JS
     watch('src/scss/**/*.scss', css); // Vigila los archivos SCSS
     console.log('Vigilando cambios en archivos...');
 }
 
-export default series( js, css, watchFiles )
+export default series( css, watchFiles )
